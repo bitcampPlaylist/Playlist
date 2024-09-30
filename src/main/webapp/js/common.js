8b8fadc3358e80e8282ba52c52a114f6f83b7779
rwanda95do <dlsdnr2379@naver.com>
2024-09-30 10:37:31 +0900

    2222

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     // 페이지가 처음 로드될 때 main.do를 로드
 	if (!sessionStorage.getItem('mainLoaded')) {
-	    $("#main-content").load("/playlist2/main.do");
+/*	    $("#main-content").load("/playlist2/main.do");*/
+		$("#main-content").load("/playlist2/dj/djToday.do");
 	    sessionStorage.setItem('mainLoaded', 'true'); // 한 번 로드했음을 기록
 	}
     // 회원가입 버튼 클릭 시 register.do를 비동기 로드
@@ -19,7 +20,6 @@ $(document).ready(function() {
 	    event.preventDefault(); // 기본 동작 방지 (폼 제출 방지)
 	    $("#main-content").load("/playlist2/member/loginForm.do");
 	});
-	
 	
 	$(document).on("click", "#logout-btn", function() {
 		// AJAX 요청
