b62c1ba9bc148b8a6247601caa864ec09047774b
chaejunho689 <smile689@naver.com>
2024-09-30 10:35:24 +0900

    채준호 뮤직플레이어 리스트

--- src/main/java/musicplayer/dao/SongDAO.java
+++ src/main/java/musicplayer/dao/SongDAO.java
@@ -33,9 +33,10 @@ public class SongDAO {
 		}
 	}
 
-    	System.out.println(song_id);
+    public SonglistDTO getSongById(int id) {
+    	System.out.println(id);
     	SqlSession sqlSession = sqlSessionFactory.openSession();
-        SonglistDTO songlistDTO = sqlSession.selectOne("musicplayerSQL.loadSong", song_id);
+        SonglistDTO songlistDTO = sqlSession.selectOne("musicplayerSQL.loadSong", id);
         System.out.println(songlistDTO);
         sqlSession.close();	
         return songlistDTO;
--- src/main/java/musicplayer/service/GetSongService.java
+++ src/main/java/musicplayer/service/GetSongService.java
@@ -17,11 +17,13 @@ public class GetSongService implements CommandProcess {
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		
 		//데이터
+		int id = Integer.parseInt(request.getParameter("song_id"));
+//		String id = request.getParameter("song_id");
 		
 		//DB
 		SongDAO songDAO = SongDAO.getInstance();
 		SonglistDTO songlistDTO = songDAO.getSongById(id);
-		//int song_id_int = songDAO.getSongById(id);
+//		int song_id_int = songDAO.getSongById(id);
 		
 		// JSON 형식으로 응답
 		response.setContentType("application/json");
